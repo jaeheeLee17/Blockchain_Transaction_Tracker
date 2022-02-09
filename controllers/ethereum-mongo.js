5ae0116a3ad73fd496633ddef53e00611ccd9531
jaeheeLee17 <ipod031@naver.com>
2022-02-09 20:18:43 +0900

    add extra APIs dealing with DB

---
+++ controllers/ethereum-mongo.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const ethTransactions = require('../models/ethTransactions');
+const ethTokens = require('../models/ethTokens');
+const cwr = require('../utils/createWebResponse');
+
+const getTxFrom = async (req, res) => {
+  const header = res.setHeader('Content-Type', 'application/json');
+  try {
+    const {source} = req.query;
+    const TxFromList = await ethTransactions.find({"from": source.toLowerCase()});
+    return cwr.createWebResp(res, header, 200, TxFromList);
+  } catch (e) {
+    return cwr.errorWebResp(res, header, 500,
+      'get Transactions with source address failed', e.message || e);
+  }
+}
+
+const getTxTo = async (req, res) => {
+  const header = res.setHeader('Content-Type', 'application/json');
+  try {
+    const {destination} = req.query;
+    const TxToList = await ethTransactions.find({"to": destination.toLowerCase()});
+    return cwr.createWebResp(res, header, 200, TxToList);
+  } catch (e) {
+    return cwr.errorWebResp(res, header, 500,
+      'get Transactions with destination address failed', e.message || e);
+  }
+}
+
+const getTokenTxFrom = async (req, res) => {
+  const header = res.setHeader('Content-Type', 'application/json');
+  try {
+    const {source} = req.query;
+    const TokenTxFromList = await ethTokens.find({"from": source.toLowerCase()});
+    return cwr.createWebResp(res, header, 200, TokenTxFromList);
+  } catch (e) {
+    return cwr.errorWebResp(res, header, 500,
+      'get Transactions with source address failed', e.message || e);
+  }
+}
+
+const getTokenTxTo = async (req, res) => {
+  const header = res.setHeader('Content-Type', 'application/json');
+  try {
+    const {destination} = req.query;
+    const TokenTxToList = await ethTokens.find({"to": destination.toLowerCase()});
+    return cwr.createWebResp(res, header, 200, TokenTxToList);
+  } catch (e) {
+    return cwr.errorWebResp(res, header, 500,
+      'get Transactions with destination address failed', e.message || e);
+  }
+}
+
+module.exports = {
+  getTxFrom,
+  getTxTo,
+  getTokenTxFrom,
+  getTokenTxTo,
+}
