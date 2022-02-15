655d6cd478372bbe6b808ac972100981b47e20e1
222yujin <222yujin@naver.com>
2022-02-15 22:04:19 +0900

    gitignore && init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edfdbb8f976b6b85"; }
+/******/ 		__webpack_require__.h = function() { return "2c7c0d3df3432de5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/dashboard/node_connect.js
+++ src/components/dashboard/node_connect.js
@@ -1,8 +1,6 @@
-import { Bar } from "react-chartjs-2";
+import { Graph } from "react-d3-graph";
 import React from "react";
-import { ResponsiveNetwork } from "@nivo/network";
-import node_graph from "./node_graph";
-import { ForceGraph, ForceGraphNode, ForceGraphLink } from "react-vis-force";
+// import node_graph from "./node_graph";
 import {
   Box,
   Button,
@@ -16,90 +14,186 @@ import {
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 export const Sales = (props) => {
-  const theme = useTheme();
-
   const data = {
-    datasets: [
+    links: [
       {
-        backgroundColor: "#3F51B5",
-        barPercentage: 0.5,
-        barThickness: 12,
-        borderRadius: 4,
-        categoryPercentage: 0.5,
-        data: [18, 5, 19, 27, 29, 19, 20],
-        label: "This year",
-        maxBarThickness: 10,
+        source: 1,
+        target: 2,
+        label: "link 1 and 2",
       },
       {
-        backgroundColor: "#EEEEEE",
-        barPercentage: 0.5,
-        barThickness: 12,
-        borderRadius: 4,
-        categoryPercentage: 0.5,
-        data: [11, 20, 12, 29, 30, 25, 13],
-        label: "Last year",
-        maxBarThickness: 10,
+        source: 1,
+        target: 3,
+      },
+      {
+        source: 1,
+        target: 4,
+      },
+      {
+        source: 3,
+        target: 4,
+        breakPoints: [
+          { x: 100, y: 20 },
+          { x: 20, y: 100 },
+        ],
+      },
+      {
+        source: 4,
+        target: 4,
       },
     ],
-    labels: ["1 Aug", "2 Aug", "3 Aug", "4 Aug", "5 Aug", "6 Aug", "7 aug"],
+    nodes: [
+      {
+        id: 1,
+        name: "Node 1",
+      },
+      {
+        id: 2,
+        name: "Node 2",
+      },
+      {
+        id: 3,
+        name: "Node 3",
+      },
+      {
+        id: 4,
+        name: "Node 4",
+      },
+    ],
   };
 
-  const options = {
-    animation: false,
-    cornerRadius: 20,
-    layout: { padding: 0 },
-    legend: { display: false },
-    maintainAspectRatio: false,
-    responsive: true,
-    xAxes: [
-      {
-        ticks: {
-          fontColor: theme.palette.text.secondary,
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false,
-        },
-      },
-    ],
-    yAxes: [
-      {
-        ticks: {
-          fontColor: theme.palette.text.secondary,
-          beginAtZero: true,
-          min: 0,
-        },
-        gridLines: {
-          borderDash: [2],
-          borderDashOffset: [2],
-          color: theme.palette.divider,
-          drawBorder: false,
-          zeroLineBorderDash: [2],
-          zeroLineBorderDashOffset: [2],
-          zeroLineColor: theme.palette.divider,
-        },
-      },
-    ],
-    tooltips: {
-      backgroundColor: theme.palette.background.paper,
-      bodyFontColor: theme.palette.text.secondary,
-      borderColor: theme.palette.divider,
-      borderWidth: 1,
-      enabled: true,
-      footerFontColor: theme.palette.text.secondary,
-      intersect: false,
-      mode: "index",
-      titleFontColor: theme.palette.text.primary,
+  // the graph configuration, just override the ones you need
+  // const myConfig = {
+  //   automaticRearrangeAfterDropNode: false,
+  //   collapsible: false,
+  //   height: 400,
+  //   highlightDegree: 1,
+  //   highlightOpacity: 0.2,
+  //   linkHighlightBehavior: true,
+  //   maxZoom: 8,
+  //   minZoom: 0.1,
+  //   nodeHighlightBehavior: true,
+  //   panAndZoom: false,
+  //   staticGraph: false,
+  //   width: 800,
+  //   node: {
+  //     color: "#4caf50",
+  //     fontColor: "black",
+  //     fontSize: 12,
+  //     fontWeight: "normal",
+  //     highlightColor: "red",
+  //     highlightFontSize: 12,
+  //     highlightFontWeight: "bold",
+  //     highlightStrokeColor: "SAME",
+  //     highlightStrokeWidth: 1.5,
+  //     labelProperty: "name",
+  //     mouseCursor: "pointer",
+  //     opacity: 1,
+  //     renderLabel: true,
+  //     size: 450,
+  //     strokeColor: "none",
+  //     strokeWidth: 1.5,
+  //     svg: "",
+  //     symbolType: "circle",
+  //   },
+  //   link: {
+  //     color: "#4caf50",
+  //     fontColor: "red",
+  //     fontSize: 10,
+  //     highlightColor: "blue",
+  //     highlightFontWeight: "bold",
+  //     labelProperty: (link) => `from ${link.source} to ${link.target}`,
+  //     opacity: 1,
+  //     renderLabel: false,
+  //     semanticStrokeWidth: false,
+  //     strokeWidth: 4,
+  //   },
+  //   d3: {
+  //     gravity: -400,
+  //     linkLength: 300,
+  //   },
+  // };
+  const myConfig = {
+    automaticRearrangeAfterDropNode: false,
+    collapsible: false,
+    directed: false,
+    focusAnimationDuration: 0.75,
+    focusZoom: 1,
+    freezeAllDragEvents: false,
+    height: 400,
+    highlightDegree: 1,
+    highlightOpacity: 1,
+    linkHighlightBehavior: false,
+    maxZoom: 8,
+    minZoom: 0.1,
+    nodeHighlightBehavior: false,
+    panAndZoom: false,
+    staticGraph: false,
+    staticGraphWithDragAndDrop: false,
+    width: 800,
+    d3: {
+      alphaTarget: 0.05,
+      gravity: -100,
+      linkLength: 100,
+      linkStrength: 1,
+      disableLinkForce: false,
     },
+    node: {
+      color: "#4caf50",
+      fontColor: "black",
+      fontSize: 8,
+      fontWeight: "normal",
+      highlightColor: "SAME",
+      highlightFontSize: 8,
+      highlightFontWeight: "normal",
+      highlightStrokeColor: "SAME",
+      highlightStrokeWidth: "SAME",
+      labelProperty: "id",
+      mouseCursor: "pointer",
+      opacity: 1,
+      renderLabel: true,
+      size: 200,
+      strokeColor: "none",
+      strokeWidth: 1.5,
+      svg: "",
+      symbolType: "circle",
+    },
+    link: {
+      color: "#d3d3d3",
+      fontColor: "black",
+      fontSize: 8,
+      fontWeight: "normal",
+      highlightColor: "SAME",
+      highlightFontSize: 8,
+      highlightFontWeight: "normal",
+      labelProperty: "label",
+      mouseCursor: "pointer",
+      opacity: 1,
+      renderLabel: false,
+      semanticStrokeWidth: false,
+      strokeWidth: 1.5,
+      markerHeight: 6,
+      markerWidth: 6,
+      strokeDasharray: 0,
+      strokeDashoffset: 0,
+      strokeLinecap: "butt",
+    },
+  };
+  const onClickNode = function (nodeId) {
+    window.alert(`Clicked node ${nodeId}`);
+  };
+
+  const onClickLink = function (source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
   };
 
   return (
-    <Card {...props}>
-      <CardHeader title="Node connection graph" />
-      {/* 거래량에 따른 그래프 형태 변화 선택 기능 */}
+      <Card {...props}>
+        <CardHeader title="Node connection graph" />
+        {/* 거래량에 따른 그래프 형태 변화 선택 기능 */}
 
-      <Divider />
-      {/* 
+        <Divider />
+        {/*
       <img
         style={{
           alignItems: "center",
@@ -109,17 +203,15 @@ export const Sales = (props) => {
         }}
         src="/static/images/node.png"
       /> */}
-      <ForceGraph simulationOptions={{ height: 300, width: 300 }}>
-        <ForceGraphNode node={{ id: "first-node" }} fill="green" />
-        <ForceGraphNode node={{ id: "second-node" }} fill="green" />
-        <ForceGraphNode node={{ id: "third-node" }} fill="green" />
-        <ForceGraphNode node={{ id: "fourth-node" }} fill="green" />
-        <ForceGraphLink
-          link={{ source: "first-node", target: "second-node" }}
+        <Graph
+            id="graph-id" // id is mandatory
+            data={data}
+            config={myConfig}
+            onClickNode={onClickNode}
+            onClickLink={onClickLink}
         />
-      </ForceGraph>
-      <node_graph />
-      {/* <CardContent>
+        <node_graph />
+        {/* <CardContent>
         <Box
           sx={{
             height: 400,
@@ -129,22 +221,22 @@ export const Sales = (props) => {
           <Bar data={data} options={options} />
         </Box>
       </CardContent> */}
-      <Divider />
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-end",
-          p: 2,
-        }}
-      >
-        <Button
-          color="primary"
-          endIcon={<ArrowRightIcon fontSize="small" />}
-          size="small"
+        <Divider />
+        <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              p: 2,
+            }}
         >
-          More Details
-        </Button>
-      </Box>
-    </Card>
+          <Button
+              color="primary"
+              endIcon={<ArrowRightIcon fontSize="small" />}
+              size="small"
+          >
+            More Details
+          </Button>
+        </Box>
+      </Card>
   );
 };
