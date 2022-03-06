10963c0914193a560604b07311788453f6c20b2c
jaeheeLee17 <ipod031@naver.com>
2022-03-06 15:48:17 +0900

    rearrange and apply some modules

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const ethRouter = require('./routes/api/eth');
@@ -20,8 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-const url = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}`;
+const url = `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}`;
 const connect = mongoose.connect(url, {useNewUrlParser: true});
 
 connect.then(() => {
