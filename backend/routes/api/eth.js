520de7b2897690aa9efdc835dabb591d19aed431
warmth424 <56920883+warmth424@users.noreply.github.com>
2022-03-10 06:59:28 +0900

    error correction

--- backend/routes/api/eth.js
+++ backend/routes/api/eth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // router.post('/network/block', EthController.postBlockInfo);
 router.post('/network/transaction', mw.web3, EthController.postTransactionInfo);
-router.post('/network/txlistchain', mw.etherscan, EthController.postTxlistChainWithAddress);
+router.post('/network/txlistchain', mw.etherscan, mw.web3, EthController.postTxlistChainWithAddress);
 router.post('/network/tokenTxlist', mw.etherscan, mw.web3, EthController.postTokenTxListWithAddress);
 
 router.get('/network/etherBalance', mw.web3, EthController.getEtherBalance);
