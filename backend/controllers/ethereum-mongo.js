e0cc442710746d5048b9697b98c3e0e7fee51682
jaeheeLee17 <ipod031@naver.com>
2022-03-09 15:55:42 +0900

    make new api for caching ethereum transaction chain from DB

--- backend/controllers/ethereum-mongo.js
+++ backend/controllers/ethereum-mongo.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const ethTransactions = require('../models/ethTransactions');
 const ethTokens = require('../models/ethTokens');
+const eth_tx_traces = require('../models/eth_transactions_trace');
 const cwr = require('../utils/createWebResponse');
 
 const getTxFrom = async (req, res) => {
@@ -9,6 +10,18 @@ const getTxFrom = async (req, res) => {
     const {source} = req.query;
     const TxFromList = await ethTransactions.find({"from": source});
     return cwr.createWebResp(res, header, 200, TxFromList);
+  } catch (e) {
+    return cwr.errorWebResp(res, header, 500,
+      'get Transactions with source address failed', e.message || e);
+  }
+}
+
+const getTxChainFrom = async (req, res) => {
+  const header = res.setHeader('Content-Type', 'application/json');
+  try {
+    const {source} = req.query;
+    const TxChainFromList = await eth_tx_traces.find({"from": source});
+    return cwr.createWebResp(res, header, 200, TxChainFromList);
   } catch (e) {
     return cwr.errorWebResp(res, header, 500,
       'get Transactions with source address failed', e.message || e);
@@ -53,6 +66,7 @@ const getTokenTxTo = async (req, res) => {
 
 module.exports = {
   getTxFrom,
+  getTxChainFrom,
   getTxTo,
   getTokenTxFrom,
   getTokenTxTo,
--- backend/routes/api/eth.js
+++ backend/routes/api/eth.js
@@ -12,6 +12,7 @@ router.post('/network/tokenTxlist', EthController.postTokenTxListWithAddress);
 router.get('/network/etherBalance', EthController.getEtherBalance);
 router.get('/network/tokenBalanceList', EthController.getTokenBalanceList);
 router.get('/db/TxFrom', DBController.getTxFrom);
+router.get('/db/TxChainFrom', DBController.getTxChainFrom);
 router.get('/db/TxTo', DBController.getTxTo);
 router.get('/db/TokenTxFrom', DBController.getTokenTxFrom);
 router.get('/db/TokenTxTo', DBController.getTokenTxTo);
