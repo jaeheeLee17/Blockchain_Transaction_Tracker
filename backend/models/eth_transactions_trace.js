bde098e6421a51b4ef2c1da628115406ba262bef
jaeheeLee17 <ipod031@naver.com>
2022-03-09 15:38:40 +0900

    make db schema for etherum transaction chains

---
+++ backend/models/eth_transactions_trace.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Eth_tx_trace_schema = new Schema({
+  from: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  startBlockNumber: {
+    type: String,
+    required: true
+  },
+  endBlockNumber: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: Array,
+    required: true
+  },
+  to_related: [
+    [
+      {
+        tx: {
+          type: String,
+          required: true
+        },
+        data: {
+          from: {
+            type: String,
+            required: true
+          },
+          to: {
+            type: String,
+            required: true
+          },
+          value: {
+            type: String,
+            required: true
+          }
+        }
+      }
+    ]
+  ]
+}, {
+  timestamps: true
+})
+
+const eth_tx_traces = mongoose.model('eth_tx_traces', Eth_tx_trace_schema);
+
+module.exports = eth_tx_traces
