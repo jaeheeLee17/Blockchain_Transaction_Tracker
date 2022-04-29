1f09df024ea100217ead0295dd679842fb144440
Bakjiu <jxxwxxx@gmail.com>
2022-04-29 16:25:00 +0900

    overview page add

--- frontend/src/pages/transactiondetail.js
+++ frontend/src/pages/transactiondetail.js
@@ -1,53 +1,49 @@
 import Head from "next/head";
-import {useRouter} from 'next/router';
-import {DashboardLayout} from "../components/dashboard-layout";
-import {Box, Container, Typography} from "@mui/material";
-import {Overview} from "../components/transaction/overview";
+import { useRouter } from "next/router";
+import { DashboardLayout } from "../components/dashboard-layout";
+import { Box, Container, Typography } from "@mui/material";
+import { Overview } from "../components/transaction/overview";
 import TransactionTab from "../components/transaction/transactionTab";
 
-
-const Transactiondetail = () => {
-
-
-const router = useRouter();
-console.log(router.query)
-console.log('node to: ', router.query.to);
-const to = router.query.to;
-
-return(
+const Transactiondetail = (props) => {
+  const router = useRouter();
+  console.log(router.query);
+  const data = JSON.parse(router.query.data);
+  console.log(data);
+  return (
     <>
-        <Head>
-            <title>Transaction Detail</title>
-        </Head>
+      <Head>
+        <title>Transaction Detail</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <Typography sx={{ m: 1 }} variant="h4">
+            Transaction Details
+          </Typography>
+        </Container>
         <Box
-            component="main"
-            sx={{
-                flexGrow: 1,
-                py: 8,
-            }}
+          component="main"
+          sx={{
+            flexGrow: 1,
+            py: 4,
+          }}
         >
-            <Container maxWidth={false}>
-                <Typography sx={{m: 1}} variant="h4">
-                    Transaction Details
-                </Typography>
-            </Container>
-            <Box
-                component="main"
-                sx={{
-                    flexGrow: 1,
-                    py: 4,
-                }}
-            >
-                <Container maxWidth="sx">
-                    <TransactionTab/>
-                </Container>
-            </Box>
+          <Container maxWidth="sx">
+            <TransactionTab />
+          </Container>
         </Box>
+      </Box>
     </>
-);
-}
+  );
+};
 
 Transactiondetail.getLayout = (page) => (
-    <DashboardLayout>{page}</DashboardLayout>
+  <DashboardLayout>{page}</DashboardLayout>
 );
 export default Transactiondetail;
